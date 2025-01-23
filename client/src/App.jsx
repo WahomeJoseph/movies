4cde5a036d3d5ea239a061808c672cb4b9d6f3fe
wahomeJohn <johnweru206@gmail.com>
2025-01-23 14:25:44 +0300

    123

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
+
+import { Navbar } from './components/Navbar'
+import { Moviecard } from './components/MovieCard'
 
 export default function App() {
   return (
-    <div></div>
+    <Router>
+     <Navbar/>
+     <Routes>
+      <Route path='/movie' element={<Moviecard/>}/>
+     </Routes>
+    </Router>
   )
 }
 
--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -4,10 +4,10 @@ export const Moviecard = ({movie}) => {
 
   return (
     <div className='flex flex-col relative bg-[#1a1a1a] h-screen transition transform duration-200 overflow-hidden text-base md:text-sm'>
-        <div className=''>
+        <div className='bg-blend-color-dodge'>
             <img src="" alt="movie-thumb" />
             <div className='absolute flex flex-col justify-end inset-0 bg-[#000] opacity-0 transition-opacity duration-200 p-4'>
-                <button onClick={handleFav} className=''></button>
+                <button onClick={handleFav} className='bg-cyan-400 blur-lg'></button>
             </div>
         </div>
         <div className=''>
