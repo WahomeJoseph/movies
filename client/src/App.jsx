cfd0b44d6fd14d8c45da9e5a7fe87f3f881c3d6a
wahomeJohn <johnweru206@gmail.com>
2025-01-23 15:19:54 +0300

    123

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 import { Navbar } from './components/Navbar'
 import { Moviecard } from './components/MovieCard'
