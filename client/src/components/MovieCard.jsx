c2454215547bd210915f071c5974ca6230943ea9
wahomeJohn <johnweru206@gmail.com>
2025-01-23 17:37:39 +0300

    123

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 
 export const Moviecard = ({movie}) => {
 
   return (
-    <div className='flex flex-col relative bg-[#1a1a1a] h-screen transition transform duration-200 overflow-hidden text-base md:text-sm'>
+    <div className='flex flex-col relative bg-red-600 h-screen transition transform duration-200 overflow-hidden text-base md:text-sm'>
         <div className='bg-blend-color-dodge'>
             <img src="" alt="movie-thumb" />
             <div className='absolute flex flex-col justify-end inset-0 bg-[#000] opacity-0 transition-opacity duration-200 p-4'>
