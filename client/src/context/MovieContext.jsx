8e1f7e4caf6e01d74806b549ec22133b3309396b
wahomeJohn <johnweru206@gmail.com>
2025-01-24 12:29:17 +0300

    12..

--- client/src/context/MovieContext.jsx
+++ client/src/context/MovieContext.jsx
@@ -1,42 +1,43 @@
+
 import { createContext, useState, useEffect, useContext } from "react"
 import PropTypes from 'prop-types'
 
 const MovieContext = createContext()
-
 export const useMovieContext = () => useContext(MovieContext)
 
 export const MovieProvider = ({children}) => {
-    const [favorites, setFavorites] = useState([])
+    const [favs, setFavs] = useState([])
 
     useEffect(() => {
-        const storedFavs = localStorage.getItem("favorites")
-        if (storedFavs) setFavorites(JSON.parse(storedFavs))
+        const storedFavs = localStorage.getItem("favorites") 
+        if (storedFavs) setFavs(JSON.parse(storedFavs)) //check for any favs in local storage
     }, [])
 
     useEffect(() => {
-        localStorage.setItem('favorites', JSON.stringify(favorites))
-    }, [favorites])
+        localStorage.setItem('favorites', JSON.stringify(favs)) //update the favs in local storage in string format
+    }, [favs])
 
-    const addToFavorites = (movie) => {
-        setFavorites(prev => [...prev, movie])
+    // add the movie to the favs array
+    const addToFavs = (movie) => {
+        setFavs(prev => [...prev, movie])
+    }
+    // remove the movie from the favs array
+    const removeFromFavs = (movieId) => {
+        setFavs(prev => prev.filter(movie => movie.Id !== movieId))
+    }
+    // check if the movie is in the favs array
+    const isFavorite = (movieId) => {
+        return favs.some(movie => movie.id === movieId)
     }
     
     MovieProvider.propTypes = {
         children: PropTypes.node.isRequired
     }
     
-    const removeFromFavorites = (movieId) => {
-        setFavorites(prev => prev.filter(movie => movie.id !== movieId))
-    }
-    
-    const isFavorite = (movieId) => {
-        return favorites.some(movie => movie.id === movieId)
-    }
-
     const value = {
-        favorites,
-        addToFavorites,
-        removeFromFavorites,
+        favs,
+        addToFavs,
+        removeFromFavs,
         isFavorite
     }
 
