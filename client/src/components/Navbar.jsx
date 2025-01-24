b7e2d859d46a42ca43f26496e220a7179d5393ec
wahomeJohn <johnweru206@gmail.com>
2025-01-24 09:06:24 +0300

    12..

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
     return (
-        <nav className="bg-black p-4 md:p-2 flex justify-between items-center shadow-md">
-            <div className="text-xl font-bold md:text-lg">
-                <Link to="/">Cinema</Link>
-            </div>
-            <div className="flex gap-8 md:gap-4">
-                <Link to="/" className="text-base p-2 rounded transition-colors duration-200 hover:bg-white/10">Home</Link>
-                <Link to="/favorites" className="text-base p-2 rounded transition-colors duration-200 hover:bg-white/10">Favorites</Link>
+        <nav className="bg-black p-4 flex justify-between items-center shadow-md shadow-[#cccccc]  md:p-2 md:flex sm:text-base">
+            <span className="text-xl font-bold md:text-lg">
+                <Link to="/">cinema</Link>
+            </span>
+            <div className="flex items-center gap-8 md:gap-4 md:p-4">
+                <Link to="/" className="text-base p-2 rounded duration-200 hover:bg-white/10">Home</Link>
+                <Link to="/favorites" className="text-base p-2 rounded duration-200 hover:bg-white/10">Favorites</Link>
             </div>
         </nav>
     );
