18afec8410365403daa826ed3ca1550eb524fdec
wahomeJohn <johnweru206@gmail.com>
2025-01-26 14:10:42 +0300

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
     return (
-        <nav className="w-full bg-[#030f0f] flex justify-between gap-2 px-20 items-center shadow-md shadow-[#030f0f] text-base sm:flex-row sm:mt-0 sm:justify-between sm:gap-4 md:flex-row sm:text-base">
-            <span className="text-xl text-[#00df82] font-bold sm:text-xl md:text-lg">
-                <Link to="/">cinema</Link>
+        <nav className="flex w-full bg-[#030f0f] justify-between gap-2 px-8 items-center shadow-md shadow-[#030f0f] text-base sm:flex-row sm:mt-0 sm:justify-between sm:gap-4 md:flex-row sm:text-base">
+            <span className="text-lg text-[#00df82] font-bold sm:text-xl md:text-lg">
+                <Link to="/" className="italic uppercase">cinema</Link>
             </span>
             <div className="flex justify-between items-center gap-4 my-2 md:gap-4 md:p-4">
-                <Link to="/" className=" p-2 text-[#00df82] rounded-sm text-sm sm:text-base hover:shadow-[#00df82] hover:shadow-sm lg:p-4">Home</Link>
-                <Link to="/favs" className=" p-2 text-[#00df82] rounded-sm text-sm sm:text-base hover:shadow-[#00df82] hover:shadow-sm">Favorites</Link>
+                <Link to="/" className="p-2 text-[#00df82] rounded-sm text-sm sm:text-base hover:shadow-[#00df82] hover:shadow-sm lg:p-4">Home</Link>
+                <Link to="/favs" className="p-2 text-[#00df82] rounded-sm text-sm sm:text-base hover:shadow-[#00df82] hover:shadow-sm">Favorites</Link>
             </div>
         </nav>
     );
